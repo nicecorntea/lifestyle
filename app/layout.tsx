0740ec465933286c17d5a545fe083060a30e6018
SumSumCorn <a01035587180@gmail.com>
2024-07-02 15:30:42 +0900

    style: 생활습관협회 기본 hero 페이지

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import { ReactNode } from "react";
-import { Inter } from "next/font/google";
-import { Viewport } from "next";
-import PlausibleProvider from "next-plausible";
-import { getSEOTags } from "@/libs/seo";
-import ClientLayout from "@/components/LayoutClient";
-import config from "@/config";
-import "./globals.css";
+import { ReactNode } from 'react';
+import { Inter, Noto_Sans_KR } from 'next/font/google';
+import { Viewport } from 'next';
+import PlausibleProvider from 'next-plausible';
+import { getSEOTags } from '@/libs/seo';
+import ClientLayout from '@/components/LayoutClient';
+import config from '@/config';
+import './globals.css';
 
-const font = Inter({ subsets: ["latin"] });
+const font = Noto_Sans_KR({ subsets: ['latin'] });
 
 export const viewport: Viewport = {
   // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
   themeColor: config.colors.main,
-  width: "device-width",
+  width: 'device-width',
   initialScale: 1,
 };
 
@@ -22,7 +22,7 @@ export const metadata = getSEOTags();
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" data-theme={config.colors.theme} className={font.className}>
+    <html lang='kr' data-theme={config.colors.theme} className={font.className}>
       {config.domainName && (
         <head>
           <PlausibleProvider domain={config.domainName} />
