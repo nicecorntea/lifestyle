f19d63841e958e9c7b92cd69f7577c78bdba24f5
SumSumCorn <a01035587180@gmail.com>
2024-10-30 14:12:11 +0900

    update: 생코 연결

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,37 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import type { JSX } from 'react';
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
-import Image from 'next/image';
-import ButtonSignin from './ButtonSignin';
-import logo from '@/app/icon.png';
-import config from '@/config';
+import { useState, useEffect } from "react";
+import type { JSX } from "react";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import ButtonSignin from "./ButtonSignin";
+import logo from "@/app/icon.png";
+import config from "@/config";
 
 const links: {
   href: string;
   label: string;
 }[] = [
   {
-    href: '/welcome',
-    label: '협회 소개',
+    href: "/welcome",
+    label: "협회 소개",
   },
   {
-    href: '/edu',
-    label: '사업분야',
+    href: "/edu",
+    label: "사업분야",
   },
   {
-    href: '/companies',
-    label: '회원사',
+    href: "/companies",
+    label: "회원사",
+  },
+  {
+    href: "https://lifecoaching.starboy1111.xyz/",
+    label: "생활습관 코칭",
   },
 ];
 
-const cta = <ButtonSignin extraStyle='btn-primary' />;
+// const cta = <ButtonSignin extraStyle="btn-primary" />;
 // A header with a logo on the left, links in the center (like Pricing, etc...), and a CTA (like Get Started or Login) on the right.
 // The header is responsive, and on mobile, the links are hidden behind a burger button.
 const Header = () => {
@@ -40,62 +44,62 @@ const Header = () => {
   }, [searchParams]);
 
   return (
-    <header className='w-full bg-white'>
+    <header className="w-full bg-white">
       <nav
-        className='container flex items-center justify-between px-8 py-4 mx-auto'
-        aria-label='Global'
+        className="container flex items-center justify-between px-8 py-4 mx-auto"
+        aria-label="Global"
       >
         {/* Your logo/name on large screens */}
-        <div className='flex lg:flex-1'>
+        <div className="flex lg:flex-1">
           <Link
-            className='flex items-center gap-2 shrink-0'
-            href='/'
+            className="flex items-center gap-2 shrink-0"
+            href="/"
             title={`${config.appName} 홈페이지`}
           >
             <Image
               src={logo}
               alt={`${config.appName} logo`}
-              className='w-8'
+              className="w-8"
               // placeholder='blur'
               priority={true}
               width={32}
               height={32}
             />
-            <span className='text-lg'>{config.appName}</span>
+            <span className="text-lg">{config.appName}</span>
           </Link>
         </div>
         {/* Burger button to open menu on mobile */}
-        <div className='flex lg:hidden'>
+        <div className="flex lg:hidden">
           <button
-            type='button'
-            className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5'
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
             onClick={() => setIsOpen(true)}
           >
-            <span className='sr-only'>Open main menu</span>
+            <span className="sr-only">Open main menu</span>
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
               strokeWidth={1.5}
-              stroke='currentColor'
-              className='w-6 h-6 text-base-content'
+              stroke="currentColor"
+              className="w-6 h-6 text-base-content"
             >
               <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
           </button>
         </div>
 
         {/* Your links on large screens */}
-        <div className='hidden lg:flex lg:justify-center lg:gap-12 lg:items-center'>
+        <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center">
           {links.map((link) => (
             <Link
               href={link.href}
               key={link.href}
-              className='link link-hover'
+              className="link link-hover"
               title={link.label}
             >
               {link.label}
@@ -104,64 +108,64 @@ const Header = () => {
         </div>
 
         {/* CTA on large screens */}
-        <div className='hidden lg:flex lg:justify-end lg:flex-1'>{cta}</div>
+        <div className="hidden lg:flex lg:justify-end lg:flex-1"></div>
       </nav>
 
       {/* Mobile menu, show/hide based on menu state. */}
-      <div className={`relative z-50 ${isOpen ? '' : 'hidden'}`}>
+      <div className={`relative z-50 ${isOpen ? "" : "hidden"}`}>
         <div
           className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-base-200 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
         >
           {/* Your logo/name on small screens */}
-          <div className='flex items-center justify-between'>
+          <div className="flex items-center justify-between">
             <Link
-              className='flex items-center gap-2 shrink-0 '
+              className="flex items-center gap-2 shrink-0 "
               title={`${config.appName} homepage`}
-              href='/'
+              href="/"
             >
               <Image
                 src={logo}
                 alt={`${config.appName} logo`}
-                className='w-8'
+                className="w-8"
                 // placeholder='blur'
                 priority={true}
                 width={32}
                 height={32}
               />
-              <span className='text-lg font-extrabold'>{config.appName}</span>
+              <span className="text-lg font-extrabold">{config.appName}</span>
             </Link>
             <button
-              type='button'
-              className='-m-2.5 rounded-md p-2.5'
+              type="button"
+              className="-m-2.5 rounded-md p-2.5"
               onClick={() => setIsOpen(false)}
             >
-              <span className='sr-only'>Close menu</span>
+              <span className="sr-only">Close menu</span>
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
                 strokeWidth={1.5}
-                stroke='currentColor'
-                className='w-6 h-6'
+                stroke="currentColor"
+                className="w-6 h-6"
               >
                 <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M6 18L18 6M6 6l12 12'
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
             </button>
           </div>
 
           {/* Your links on small screens */}
-          <div className='flow-root mt-6'>
-            <div className='py-4'>
-              <div className='flex flex-col items-start gap-y-4'>
+          <div className="flow-root mt-6">
+            <div className="py-4">
+              <div className="flex flex-col items-start gap-y-4">
                 {links.map((link) => (
                   <Link
                     href={link.href}
                     key={link.href}
-                    className='link link-hover'
+                    className="link link-hover"
                     title={link.label}
                   >
                     {link.label}
@@ -169,9 +173,9 @@ const Header = () => {
                 ))}
               </div>
             </div>
-            <div className='divider'></div>
+            <div className="divider"></div>
             {/* Your CTA on small screens */}
-            <div className='flex flex-col'>{cta}</div>
+            <div className="flex flex-col"></div>
           </div>
         </div>
       </div>
